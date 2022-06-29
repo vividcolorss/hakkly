ee8eba6af4828b010a20a52902e44391a8cbd79b
paradox89 <72633854+89paradox@users.noreply.github.com>
2022-06-29 10:43:06 -0500

    lower not upper
    whoops

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@ cd # to home
 git clone https://github.com/89paradox/hakkly.git # clone git repo
 cd hakkly # go into it
 sudo cp ./hakkly /usr/sbin/ # copy to /usr/sbin
-sudo chmod +X /usr/sbin/hakkly # make executable
+sudo chmod +x /usr/sbin/hakkly # make executable
 echo finished!
 cd ..
 rm -rf ./installer.sh
