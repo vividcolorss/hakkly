816b4d33bc3f89ca8d1912196362e6d0dd416f09
paradox89 <72633854+89paradox@users.noreply.github.com>
2022-06-29 10:31:35 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,7 @@
+echo installing
+cd # to home
+git clone https://github.com/89paradox/hakkly.git # clone git repo
+cd hakkly # go into it
+sudo cp ./hakkly /usr/sbin/ # copy to /usr/sbin
+sudo chmod +X /usr/sbin/hakkly # make executable
+echo finished!
